5123a0717f19351d63fa4af06db6f60a582230d2
satori <mcat349@ymail.com>
2014-07-04 22:48:21 -0600

    Initial Commit

---
+++ Tutorial_Transition/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tutorial_Transition
+//
+//  Created by LV426 on 7/4/14.
+//  Copyright (c) 2014 Indiow LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
